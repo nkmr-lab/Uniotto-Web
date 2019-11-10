088247a81f69c5c3b273d01d13cb14b497dbfa99
nshhhin <shinn070110@gmail.com>
2019-11-10 17:55:15 +0900

    :sparkles: 雛形を作った

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <MainContent>
+  </MainContent>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainContent from './components/Main.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    MainContent
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import "../node_modules/bulma/bulma.sass";
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,55 @@
+<template lang="html">
+    <section class="hero is-success is-fullheight">
+    <!-- Hero head: will stick at the top -->
+    <div class="hero-head">
+        <header class="navbar">
+        <div class="container white">
+            <div class="navbar-brand">
+            <a class="navbar-item">
+                <img src="https://uniotto.org/images/uniotto_logo_color.png" alt="Logo">
+            </a>
+            <span class="navbar-burger burger" data-target="navbarMenuHeroC">
+                <span></span>
+                <span></span>
+                <span></span>
+            </span>
+            </div>
+            <div id="navbarMenuHeroC" class="navbar-menu">
+            <div class="navbar-end">
+                <a class="navbar-item is-active">
+                Home
+                </a>
+                <a class="navbar-item">
+                Apps
+                </a>
+                <a class="navbar-item">
+                API
+                </a>
+            </div>
+            </div>
+        </div>
+        </header>
+    </div>
+
+    <!-- Hero content: will be in the middle -->
+    <div class="hero-body">
+        <div class="container has-text-centered">
+        <h1 class="title">
+            Uniotto
+        </h1>
+        <h2 class="subtitle">
+            みんなで楽しむ音楽アプリ
+        </h2>
+        </div>
+    </div>
+
+    </section>
+ </template>
+ 
+ <script>
+ export default {
+ }
+ </script>
+ 
+ <style lang="css" scoped>
+ </style>
